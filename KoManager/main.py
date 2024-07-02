23d72019c9e6a9216aacfa1b045e96f186ad3437
Nof1m <gustavofernandes9530@gmail.com>
2024-07-02 14:28:43 -0300

    main.py

--- KoManager/main.py
+++ KoManager/main.py
@@ -1,1 +1,15 @@
+# main.py
+
+import sys
+from PySide6.QtWidgets import QApplication
+from ui import MangaDownloader
+
+def main():
+    app = QApplication(sys.argv)
+    mainWindow = MangaDownloader()
+    mainWindow.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
 
